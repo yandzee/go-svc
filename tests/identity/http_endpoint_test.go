3baff1e31b78d89d10d47b63144e30c0d21b2f05
Renat Tuktarov <yandzeek@gmail.com>
2025-08-14 15:31:04 +0300

    first simple test for current auth check route

--- tests/identity/http_endpoint_test.go
+++ tests/identity/http_endpoint_test.go
@@ -1,10 +1,13 @@
 package identity
 
 import (
+	"bytes"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"encoding/json"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
@@ -18,38 +21,101 @@ import (
 const (
 	AccessHeaderName  = "X-Test-Access-Token"
 	RefreshHeaderName = "X-Test-Refresh-Token"
+
+	AuthCheckURL = "/auth"
 )
 
 type TestDescriptor struct {
 	AccessTokenDuration  time.Duration
 	RefreshTokenDuration time.Duration
 	Users                []TestUser
+	Steps                []StepFn
 }
 
-func TestEndpointBuild(t *testing.T) {
-	ep := buildEndpoint(t, &TestDescriptor{
-		AccessTokenDuration:  time.Minute,
-		RefreshTokenDuration: time.Minute,
+type ResponseCheckFn func(*testing.T, *httptest.ResponseRecorder)
+type StepFn func() (*http.Request, ResponseCheckFn)
+
+func TestAuthCheckRoute(t *testing.T) {
+	runTests(t, []TestDescriptor{
+		{
+			Steps: []StepFn{
+				StepFn(func() (*http.Request, ResponseCheckFn) {
+					return makeRequest(
+						t,
+						http.MethodGet,
+						AuthCheckURL,
+						identity.TokenPair{},
+						nil,
+					), makeRespChecker(http.StatusUnauthorized)
+				}),
+			},
+		},
 	})
-
-	_ = buildEndpointRouter(t, nil, ep)
 }
 
-func buildEndpointRouter(
+func makeRespChecker(status int) ResponseCheckFn {
+	return func(t *testing.T, rr *httptest.ResponseRecorder) {
+		if rr.Code != status {
+			t.Fatalf("RespChecker: expected status %d, but got %d", status, rr.Code)
+		}
+	}
+}
+
+func makeRequest(
 	t *testing.T,
-	td *TestDescriptor,
-	ep *id_http.IdentityEndpoint[TestUser],
-) http.Handler {
+	method, url string,
+	tokens identity.TokenPair,
+	body any,
+) *http.Request {
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		t.Fatalf("makeRequest failed on body marshaling: %s", err.Error())
+	}
+
+	req, err := http.NewRequest(method, url, bytes.NewReader(bodyBytes))
+	if err != nil {
+		t.Fatalf("makeRequest failed on creaing new request: %s", err.Error())
+	}
+
+	if tokens.AccessToken != nil {
+		req.Header.Add(AccessHeaderName, tokens.AccessToken.JWTString)
+
+		c := tokens.AccessToken.AsCookie(AccessHeaderName)
+		req.AddCookie(&c)
+	}
+
+	if tokens.RefreshToken != nil {
+		req.Header.Add(RefreshHeaderName, tokens.RefreshToken.JWTString)
+	}
+
+	return req
+}
+
+func runTests(t *testing.T, tests []TestDescriptor) {
+	for _, td := range tests {
+		ep := buildEndpoint(t, &td)
+		router := buildEndpointRouter(ep)
+
+		for _, stepFn := range td.Steps {
+			resp := httptest.NewRecorder()
+			req, respChecker := stepFn()
+
+			router.ServeHTTP(resp, req)
+			respChecker(t, resp)
+		}
+	}
+}
+
+func buildEndpointRouter(ep *id_http.IdentityEndpoint[TestUser]) http.Handler {
 	r := router.NewBuilder()
 
-	r.Get("/auth", ep.Check())
+	r.Get(AuthCheckURL, ep.Check())
 	r.Get("/auth/user", ep.CurrentUser())
 	r.Post("/auth/signup", ep.Signup())
 	r.Post("/auth/signin", ep.Signin())
 	r.Post("/auth/refresh", ep.Refresh())
 
 	return stdrouter.Build(&r)
-
 }
 
 func buildEndpoint(t *testing.T, td *TestDescriptor) *id_http.IdentityEndpoint[TestUser] {
